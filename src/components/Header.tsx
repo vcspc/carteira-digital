7197a0c09de895f9b89da6c971173a31a7812561
vcspc <vitonilcius@gmail.com>
2025-01-06 20:09:44 -0300

    inicialização

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header>
+        <div className="header_container">
+            <div className="header_container_left">
+                <Link href="/perfil">
+                    <img className="header_container_left_img" src="img_perfil.svg" alt="Imagem de perfil" />
+                    <p className="header_container_left_text">Ola, José</p>
+                </Link>
+            </div>
+            <div className="header_container_right">
+                <img className="header_container_right_img" src="olho.svg" alt="ocultar" />
+                <img className="header_container_right_img" src="ajuda.svg" alt="ajuda" />
+            </div>
+        </div>
+    </header>
+  )
+}
